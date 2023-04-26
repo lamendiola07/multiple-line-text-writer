8d35b238eed0bc35d98dfe9941c722a874417e3c
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-04-26 12:11:58 +0800

    [P-3] Added Extra Codes: Imported pyfiglet

--- multiple-line-text-writer.py
+++ multiple-line-text-writer.py
@@ -2,6 +2,20 @@
 
 # Write a method in python to write multiple line of text contents into a text file mylife.txt
 
+# Extra codes for design
+import pyfiglet
+from simple_colors import *
+
+print(magenta("~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~"))
+print(yellow(pyfiglet.figlet_format("Task # 4: P-3", font = "3x5" )))
+print(magenta("~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~~ooOo~"))
+
+print(blue("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ HELLO USER! WELCOME BACK TO OUR SYSTEM ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+print(blue("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TODAY WE WILL CREATE A PROGRAM TO WRITE MULTIPLE LINE TEXT ON TXT FILES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+print(green("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ READY TO GO? ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+print(green("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ - - - INITIATING SEQUENCE - - - ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"))
+
+
 #Created variable to store contents to the text file
 with open("mylife.txt", "a+") as my_File:
     option_yes = "yes"
